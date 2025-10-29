dc322b0bcbe72043440add448424dd4d6cdc547f
Diana Rocio Ramos Rodrigo <dramosrod@unsa.edu.pe>
2025-10-29 11:44:38 -0500

    ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,3 +1,17 @@
+ // ------------------------------------------------------------
+// LABORATORIO 13 - JAVASCRIPT: ESTRUCTURAS DE DATOS
+// ------------------------------------------------------------
+// EJERCICIO 4
+// Enunciado:
+// Crea una función llamada `filtrarYTransformar(arr)` que reciba
+// un arreglo de números y:
+//  • Elimine los valores negativos
+//  • Eleve los restantes al cuadrado
+//  • Devuelva la suma total de esos cuadrados
+//
+// Ejemplo:
+// filtrarYTransformar([2, -3, 4, -1, 5]); // Resultado esperado: 45
+// ------------------------------------------------------------
 function filtrarYTransformar(arr) { 
     if (!Array.isArray(arr)) {
         console.log("Error: se esperaba un arreglo de números.");
